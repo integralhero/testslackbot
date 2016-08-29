4995056eb8160e9a08a2fa0375d45cff7103e525
David Jiang <integralhero@gmail.com>
2016-08-29 11:41:35 -0700

    regex still not working

--- loraxbot.rb
+++ loraxbot.rb
@@ -10,14 +10,14 @@ class LoraxBot < SlackRubyBot::Bot
   	return user_id + Time.now.strftime("%m/%d/%Y")
   end
   
-  match '/(?<message>.*)/' do |client, data, match|
+  match '/^(?<message>.*)$/' do |client, data, match|
     client.say(text: "Hello there, you said #{match[:message]}!", channel: data.channel)
-    logger.info generate_session_id(data)
+    logger.info " Generated session_id: #{generate_session_id(data)}"
   end
 
   match /^How is the weather in (?<location>\w*)\?$/ do |client, data, match|
     client.say(channel: data.channel, text: "The weather in #{match[:location]} is nice.")
-    logger.info generate_session_id(data)
+    logger.info " Generated session_id: #{generate_session_id(data)}"
   end
 end
 
