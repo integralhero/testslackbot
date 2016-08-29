4ddecdcef328e1572f8260e0c3aa53d591a3f49d
David Jiang <integralhero@gmail.com>
2016-08-29 14:28:21 -0700

    hello better messages

--- loraxbot.rb
+++ loraxbot.rb
@@ -8,7 +8,6 @@ Slack.configure do |config|
 end
 
 def get_user_session(id)
-	puts "Received #{id}"
 	date = Time.now.strftime("%m%d%Y")
 	return "#{id.to_s}#{date}"
 end
@@ -21,13 +20,13 @@ client.on :message do |data|
 		api_key_wit = ENV['WIT_API_TOKEN']
 		response = HTTParty.post('https://api.wit.ai/converse?', :query => {:v => '#{timenow}',:session_id => session_id, :q =>"#{data.text}"}, :headers => {"Authorization" => "Bearer #{api_key_wit}"})
 		client.message channel: data['channel'], text: "#{response.to_s}" if DEBUG_MODE
-		puts response["type"]
 		case response["type"]
 		when "msg"
+			puts "Got a message"
 			client.message channel: data['channel'], text: "#{response["msg"]}"
 		else
 			puts "None matched"
-			client.message channel: data['channel'], text: "Hi <@#{data['user']}>! You said: #{data.text}"
+			client.message channel: data['channel'], text: "Hi <@#{data['user']}>! Your command was not recognized. Try testing me with common queries"
 		end
 		
 	end
