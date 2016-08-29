c9f81884f184a0b6f6b832cf1d7490a8f3618aea
David Jiang <integralhero@gmail.com>
2016-08-29 11:18:07 -0700

    added session gen

--- loraxbot.rb
+++ loraxbot.rb
@@ -1,8 +1,15 @@
 require 'slack-ruby-bot'
 
 class LoraxBot < SlackRubyBot::Bot
-  command 'trees' do |client, data, match|
-    client.say(text: 'I speak for the trees', channel: data.channel)
+
+  def generate_session_id(data)
+  	user_id = data['user']['id']
+  	return user_id + Time.now.strftime("%m/%d/%Y")
+  end
+  
+  command '' do |client, data, match|
+    client.say(text: 'Hello there!', channel: data.channel)
+    logger.info generate_session_id(data)
   end
 end
 
