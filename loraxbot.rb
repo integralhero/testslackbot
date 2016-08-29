dee9c6dee29142ccac2e9d7774b45172a6172ff4
David Jiang <integralhero@gmail.com>
2016-08-29 13:30:06 -0700

    add logging

--- loraxbot.rb
+++ loraxbot.rb
@@ -19,6 +19,7 @@ client.on :message do |data|
 	session_id = get_user_session(data['user'])
 	puts data.inspect
 	response = HTTParty.post('https://api.wit.ai/converse?', :query => {:session_id => "#{session_id}", :q =>"How do I add money from a debit card?"}, :header => {"Authorization" => "Bearer #{ENV['WIT_API_WOKEN']}"})
+	puts response
 	client.message channel: data['channel'], text: "Hi <@#{data['user']}>!"
 end
 client.start!
