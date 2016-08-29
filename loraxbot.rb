13fbd0341c3e565b648ae4de1455d0999c68d104
David Jiang <integralhero@gmail.com>
2016-08-29 12:01:11 -0700

    added client start

--- loraxbot.rb
+++ loraxbot.rb
@@ -12,6 +12,7 @@ client = Slack::RealTime::Client.new
 client.on :message do |data|
 	client.message channel: data['channel'], text: "Hi <@#{data['user']}>!"
 end
+client.start!
 
 # class LoraxBot < SlackRubyBot::Bot
 
