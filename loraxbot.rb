5432bf469d5c96b77585469d491eb7c8bf0b45a6
David Jiang <integralhero@gmail.com>
2016-08-29 11:33:20 -0700

    try a test match

--- loraxbot.rb
+++ loraxbot.rb
@@ -7,8 +7,13 @@ class LoraxBot < SlackRubyBot::Bot
   	return user_id + Time.now.strftime("%m/%d/%Y")
   end
   
-  match '/(?<message>((\w* ?)*))/' do |client, data, match|
-    client.say(text: "Hello there, you said #{match[:location]}!", channel: data.channel)
+  match '/^(?<message>((\w* ?)*))$/' do |client, data, match|
+    client.say(text: "Hello there, you said #{match[:message]}!", channel: data.channel)
+    logger.info generate_session_id(data)
+  end
+
+  match /^How is the weather in (?<location>\w*)\?$/ do |client, data, match|
+    client.say(channel: data.channel, text: "The weather in #{match[:location]} is nice.")
     logger.info generate_session_id(data)
   end
 end
