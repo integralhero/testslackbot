0cf21bc55a22d470a806af9b47d1a7fbbd9e3240
David Jiang <integralhero@gmail.com>
2016-08-29 12:10:21 -0700

    more testing

--- loraxbot.rb
+++ loraxbot.rb
@@ -10,7 +10,7 @@ end
 
 client = Slack::RealTime::Client.new
 client.on :message do |data|
-	client.message channel: data['channel'], text: "#{data.user}"
+	client.message channel: data['channel'], text: "<#{data['user']}>"
 
 	client.message channel: data['channel'], text: "Hi <@#{data['user']}>!"
 end
