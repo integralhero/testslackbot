1b8dcc56cecc46d024569fe2da9c578cf2e92cc3
David Jiang <integralhero@gmail.com>
2016-08-29 13:27:17 -0700

    change to hash rockets

--- loraxbot.rb
+++ loraxbot.rb
@@ -18,7 +18,7 @@ client.on :message do |data|
 	return if data['user'] == CHIMEBOT_ID
 	session_id = get_user_session(data['user'])
 	puts data.inspect
-	response = HTTParty.post('https://api.wit.ai/converse?', body: {"session_id": "#{session_id}", "q": "How do I add money from a debit card?"}, headers: {"Authorization": "Bearer #{ENV['WIT_API_WOKEN']}"})
+	response = HTTParty.post('https://api.wit.ai/converse?', :query => {:session_id => "#{session_id}", :q =>"How do I add money from a debit card?"}, :header => {"Authorization" => "Bearer #{ENV['WIT_API_WOKEN']}"})
 	client.message channel: data['channel'], text: "Hi <@#{data['user']}>!"
 end
 client.start!
