bfbe06671f78dc95be7ca34baea3fb1da5d9b796
David Jiang <integralhero@gmail.com>
2016-08-29 12:02:47 -0700

    change requires

--- loraxbot.rb
+++ loraxbot.rb
@@ -1,4 +1,4 @@
-require 'slack-ruby-bot'
+require 'slack-ruby-client'
 require 'logger'
 
 
@@ -13,23 +13,3 @@ client.on :message do |data|
 	client.message channel: data['channel'], text: "Hi <@#{data['user']}>!"
 end
 client.start!
-
-# class LoraxBot < SlackRubyBot::Bot
-
-#   def self.generate_session_id(data)
-#   	user_id = data['user']['id']
-#   	return user_id + Time.now.strftime("%m/%d/%Y")
-#   end
-  
-#   match '/(?<message>.*)/s' do |client, data, match|
-#     client.say(channel: data.channel, text: "Hello there, you said #{match[:message]}!")
-#     logger.info " Generated session_id: #{LoraxBot.generate_session_id(data)}"
-#   end
-
-#   match /^How is the weather in (?<location>\w*)\?$/ do |client, data, match|
-#     client.say(channel: data.channel, text: "The weather in #{match[:location]} is nice.")
-#     logger.info " Generated session_id: #{LoraxBot.generate_session_id(data)}"
-#   end
-# end
-
-# LoraxBot.run
