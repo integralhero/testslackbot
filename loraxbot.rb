1a10f1762b202a0a27a5810601ee66a7069dc2e6
David Jiang <integralhero@gmail.com>
2016-08-29 11:58:08 -0700

    forgot auth

--- loraxbot.rb
+++ loraxbot.rb
@@ -4,6 +4,10 @@ require 'logger'
 
 logger = Logger.new(STDOUT)
 
+Slack.configure do |config|
+  config.token = ENV['SLACK_API_TOKEN']
+end
+
 client = Slack::RealTime::Client.new
 client.on :message do |data|
 	client.message channel: data['channel'], text: "Hi <@#{data['user']}>!"
