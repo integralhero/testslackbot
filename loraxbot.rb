c25cdd77068e9f94ef2bccb636ed4457cafc38ab
David Jiang <integralhero@gmail.com>
2016-08-29 11:51:35 -0700

    test regex again

--- loraxbot.rb
+++ loraxbot.rb
@@ -5,19 +5,19 @@ logger = Logger.new(STDOUT)
 
 class LoraxBot < SlackRubyBot::Bot
 
-  def generate_session_id(data)
+  def self.generate_session_id(data)
   	user_id = data['user']['id']
   	return user_id + Time.now.strftime("%m/%d/%Y")
   end
   
-  match '/^(?<message>.*)$/' do |client, data, match|
-    client.say(text: "Hello there, you said #{match[:message]}!", channel: data.channel)
-    logger.info " Generated session_id: #{generate_session_id(data)}"
+  match '/(?<message>.*)/s' do |client, data, match|
+    client.say(channel: data.channel, text: "Hello there, you said #{match[:message]}!")
+    logger.info " Generated session_id: #{LoraxBot.generate_session_id(data)}"
   end
 
   match /^How is the weather in (?<location>\w*)\?$/ do |client, data, match|
     client.say(channel: data.channel, text: "The weather in #{match[:location]} is nice.")
-    logger.info " Generated session_id: #{generate_session_id(data)}"
+    logger.info " Generated session_id: #{LoraxBot.generate_session_id(data)}"
   end
 end
 
