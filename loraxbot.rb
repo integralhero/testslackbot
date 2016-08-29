39cf6a2d8c5ed48568a05e67bef33e674c1c0138
David Jiang <integralhero@gmail.com>
2016-08-29 14:08:15 -0700

    get rid of unexpected return

--- loraxbot.rb
+++ loraxbot.rb
@@ -15,13 +15,14 @@ end
 
 client = Slack::RealTime::Client.new
 client.on :message do |data|
-	return if data['user'] == CHIMEBOT_ID
-	session_id = get_user_session(data['user'])
-	puts session_id
-	timenow = Time.now.strftime("%Y%m%d")
-	api_key_wit = ENV['WIT_API_TOKEN']
-	response = HTTParty.post('https://api.wit.ai/converse?', :query => {:v => '#{timenow}',:session_id => session_id, :q =>"#{data.text}"}, :headers => {"Authorization" => "Bearer #{api_key_wit}"})
-	client.message channel: data['channel'], text: "#{response.to_s}"
-	client.message channel: data['channel'], text: "Hi <@#{data['user']}>! You said: #{data.text}"
+	if data['user'] != CHIMEBOT_ID
+		session_id = get_user_session(data['user'])
+		puts session_id
+		timenow = Time.now.strftime("%Y%m%d")
+		api_key_wit = ENV['WIT_API_TOKEN']
+		response = HTTParty.post('https://api.wit.ai/converse?', :query => {:v => '#{timenow}',:session_id => session_id, :q =>"#{data.text}"}, :headers => {"Authorization" => "Bearer #{api_key_wit}"})
+		client.message channel: data['channel'], text: "#{response.to_s}"
+		client.message channel: data['channel'], text: "Hi <@#{data['user']}>! You said: #{data.text}"
+	end
 end
 client.start!
