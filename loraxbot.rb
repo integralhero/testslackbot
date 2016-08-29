bb4b40f99edcf3ca9aeef2288bbafa70599da74d
David Jiang <integralhero@gmail.com>
2016-08-29 12:06:36 -0700

    adding loop prevention

--- loraxbot.rb
+++ loraxbot.rb
@@ -10,6 +10,7 @@ end
 
 client = Slack::RealTime::Client.new
 client.on :message do |data|
+	logger.info "This message is from: #{data.user}"
 	client.message channel: data['channel'], text: "Hi <@#{data['user']}>!"
 end
 client.start!
