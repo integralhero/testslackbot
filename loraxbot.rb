69b0d6a2530b7aaa098a65a453d94636de7eb196
David Jiang <integralhero@gmail.com>
2016-08-30 12:29:12 -0700

    change nums hash to array

--- loraxbot.rb
+++ loraxbot.rb
@@ -12,7 +12,7 @@ def get_user_session(id)
 	return "#{id.to_s}#{date}"
 end
 
-nums = {1: "one", 2: "two", 3: "three", 4: "four", 5: "five", 6: "six", 7: "seven", 8: "eight", 9: "nine", 10: "ten"}
+nums = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]
 
 client = Slack::RealTime::Client.new
 
