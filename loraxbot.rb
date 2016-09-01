2e38a36b515eac8ea9ba2eb79fb41d0c0eb6241a
David Jiang <integralhero@gmail.com>
2016-09-01 15:48:45 -0700

    add stop handler

--- loraxbot.rb
+++ loraxbot.rb
@@ -80,10 +80,10 @@ client.on :reaction_added do |data|
 
 		case wit_response["type"]
 		when "stop"
-		puts "go to stop" if DEBUG_MODE
-		clear_session_context_for_user(data.user)
-		new_response = HTTParty.post('https://api.wit.ai/converse?', :query => {:v => '#{timenow}',:session_id => session_id, :q =>"#{data.text}", :context => "#{context}"}, :headers => {"Authorization" => "Bearer #{api_key_wit}"})
-		puts "GOT STOP: #{new_response.inspect}" if DEBUG_MODE
+			puts "go to stop" if DEBUG_MODE
+			clear_session_context_for_user(data.user)
+			new_response = HTTParty.post('https://api.wit.ai/converse?', :query => {:v => '#{timenow}',:session_id => session_id, :q =>"#{data.text}", :context => "#{context}"}, :headers => {"Authorization" => "Bearer #{api_key_wit}"})
+			puts "GOT STOP: #{new_response.inspect}" if DEBUG_MODE
 		else
 		end
 	end
