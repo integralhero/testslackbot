03d7b0382fad7a98de4a0ddaac34433e39a6d352
David Jiang <integralhero@gmail.com>
2016-09-01 13:28:29 -0700

    ah bad name error

--- loraxbot.rb
+++ loraxbot.rb
@@ -27,16 +27,27 @@ end
 
 # clear_session_context_for_user
 # PARAM: user_id from slack
-# returns whether session reset was successful
 def clear_session_context_for_user(user_id)
-	if sessions.key? user_id 
+	if !sessions.key? user_id 
 		sessions[user_id] = {}
-		return true
-	else
-		return false
 	end
+	sessions[user_id]["context"] = {}
 end
 
+def get_context_for_user(user_id)
+	if !sessions.key? user_id
+		sessions[user_id] = {}
+		sessions[user_id]["context"] = {}
+	else
+		if !sessions[user_id].key? "context"
+			sessions[user_id]["context"] = {}
+		end
+	end
+	return sessions[user_id]["context"]
+end
+def set_context_for_user(user_id, entities)
+	return sessions[user_id]["context"] = entities
+end
 
 def wit_converse(session_id, q, context="")
 	api_key_wit = ENV['WIT_API_TOKEN']
@@ -60,8 +71,8 @@ client.on :reaction_added do |data|
 
 		puts "#{data['user']} just added a #{reaction_name} to #{message_channel} at #{message_ts}" if DEBUG_MODE
 		user_selected = "#{sessions[data['user']][message_channel][message_ts][reaction_name]}"
-		puts text if DEBUG_MODE
-		client.message channel: data['item']['channel'], text: "User selected-> #{text}!" if DEBUG_MODE
+		puts user_selected if DEBUG_MODE
+		client.message channel: data['item']['channel'], text: "User selected-> #{user_selected}!" if DEBUG_MODE
 
 		# TODO: message Wit with corresponding message selected (user_selected)
 	end
@@ -74,13 +85,17 @@ client.on :message do |data|
 	if data['user'] != CHIMEBOT_ID
 		session_id = get_user_session(data['user'])
 		client.typing channel: data['channel']
+		context = get_context_for_user(data.user)
 
-		#response = HTTParty.post('https://api.wit.ai/converse?', :query => {:v => '#{timenow}',:session_id => session_id, :q =>"#{data.text}", :context => ""}, :headers => {"Authorization" => "Bearer #{api_key_wit}"})
-		response = wit_converse(session_id, data.text)
+		response = wit_converse(session_id, data.text, "#{context}")
 		# client.message channel: data['channel'], text: "#{response.to_s}" if DEBUG_MODE
 		puts "USER MESSAGE: #{data['text']}" if DEBUG_MODE
 		puts "Response from WIT: #{response.inspect}" if DEBUG_MODE
-		puts "RESPONSE TYPE====== #{response['type']}" if DEBUG_MODE
+
+		# update user's context if we have something in the response
+		if response.key? "entities"
+			context = set_context_for_user(data.user, response.entities)
+		end
 		case response["type"]
 		when "msg"
 			puts "Sending to client: #{response['msg']}" if DEBUG_MODE
@@ -89,17 +104,21 @@ client.on :message do |data|
 			action_name = response["action"]
 			puts response.inspect if DEBUG_MODE
 		when "merge"
-			puts "BEGIN MERGE====="
+			puts "got to merge"
 			puts response.inspect if DEBUG_MODE
+		# bot thinks convo has stopped, so flush the context and restart flow
 		when "stop"
 			puts "go to stop" if DEBUG_MODE
-			new_response = HTTParty.post('https://api.wit.ai/converse?', :query => {:v => '#{timenow}',:session_id => session_id, :q =>"#{data.text}", :context => ""}, :headers => {"Authorization" => "Bearer #{api_key_wit}"})
+			clear_session_context_for_user(data.user)
+			new_response = HTTParty.post('https://api.wit.ai/converse?', :query => {:v => '#{timenow}',:session_id => session_id, :q =>"#{data.text}", :context => "#{context}"}, :headers => {"Authorization" => "Bearer #{api_key_wit}"})
 			puts "GOT STOP: #{new_response.inspect}" if DEBUG_MODE
 		else
 			puts "None matched" if DEBUG_MODE
 			client.message channel: data['channel'], text: "Hi <@#{data['user']}>! Your command was not recognized. Try testing me with some common queries"
 		end
-		if response.key?("quickreplies")
+
+		# handles responses with quickreplies on Wit by displaying a selection list with emoji reactions
+		if response.key?("quickreplies") 
 			# puts response["quickreplies"]
 			index = 1
 			emojis = []
@@ -114,15 +133,13 @@ client.on :message do |data|
 			chatbot_response = web_client.chat_postMessage channel: data['channel'], text: "#{message}", as_user: true
 			puts "Chatbot response: #{chatbot_response.ts}"
 			for i in 0...response["quickreplies"].size
-				#HACK: session_id is set to user_id, but this should not be the case once session storage/retrieval is figured out
-				session_id = data['user']
-				sessions[session_id] = {} if !sessions.key? session_id
-				reply_text = response["quickreplies"][i]
-				puts "add emoji: #{emojis[i]} on #{chatbot_response.channel} at #{chatbot_response.ts}" if DEBUG_MODE
+				sessions[data.user] = {} if !sessions.key? session_id
+				reply_text = response["quickreplies"][i] 
+				puts "Adding emoji: #{emojis[i]} on #{chatbot_response.channel} at #{chatbot_response.ts}" if DEBUG_MODE
 				web_client.reactions_add(name: emojis[i], channel: chatbot_response.channel, timestamp: chatbot_response.ts)
 				sessions[session_id][chatbot_response.channel] = {} if !sessions[session_id].key? chatbot_response.channel
 				sessions[session_id][chatbot_response.channel][chatbot_response.ts] = {} if !sessions[session_id][chatbot_response.channel].key? chatbot_response.ts
-				puts "SESSION PRINT: #{sessions.inspect}" if DEBUG_MODE
+				# puts "SESSION PRINT: #{sessions.inspect}" if DEBUG_MODE
 				sessions[session_id][chatbot_response.channel][chatbot_response.ts][emojis[i]] = reply_text
 				
 			end
