aa0d331285629e18043c32a98f09efccc8ba3421
David Jiang <integralhero@gmail.com>
2016-09-02 12:04:38 -0700

    update new resp

--- loraxbot.rb
+++ loraxbot.rb
@@ -187,6 +187,7 @@ client.on :message do |data|
 			new_response = HTTParty.post('https://api.wit.ai/converse?', :query => {:v => '#{timenow}',:session_id => session_id, :q =>"#{data.text}", :context => "#{context}"}, :headers => {"Authorization" => "Bearer #{api_key_wit}"})
 			puts "GOT STOP: #{new_response.inspect}" if DEBUG_MODE
 			if new_response["type"] == "msg"
+				response = new_response
 				client.typing channel: data['channel']
 				puts "Sending to client: #{new_response['msg']}" if DEBUG_MODE
 				client.message channel: data['channel'], text: "#{new_response["msg"]}"
