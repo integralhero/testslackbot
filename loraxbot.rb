e9f71ba022f09616cdd20c82c2bbafaef4e121f5
David Jiang <integralhero@gmail.com>
2016-09-02 12:06:28 -0700

    More DRY

--- loraxbot.rb
+++ loraxbot.rb
@@ -197,35 +197,37 @@ client.on :message do |data|
 			client.message channel: data['channel'], text: "Hi <@#{data['user']}>! Your command was not recognized. Try testing me with some common queries"
 		end
 
+
+		post_quickreplies(response, data)
 		# handles responses with quickreplies on Wit by displaying a selection list with emoji reactions
-		if response.key?("quickreplies") 
-			# puts response["quickreplies"]
-			index = 1
-			emojis = []
-			message = "Please select one of the following options for further inquiries: \n"
-			for reply in response["quickreplies"] do
-				num_as_emoji = ":#{nums[index]}:"
-				option_str = "#{num_as_emoji} #{reply} \n"
-				message += option_str
-				emojis.push("#{nums[index]}")
-				index += 1
-			end
-			chatbot_response = web_client.chat_postMessage channel: data['channel'], text: "#{message}", as_user: true
-			puts "Chatbot response: #{chatbot_response.ts}"
-			for i in 0...response["quickreplies"].size
-				session_id = data.user
-				reply_text = response["quickreplies"][i] 
-				puts "Adding emoji: #{emojis[i]} on #{chatbot_response.channel} at #{chatbot_response.ts}" if DEBUG_MODE
-				web_client.reactions_add(name: emojis[i], channel: chatbot_response.channel, timestamp: chatbot_response.ts)
-				$sessions[session_id] = {} if !$sessions.key? session_id
-				$sessions[session_id][chatbot_response.channel] = {} if !$sessions[session_id].key? chatbot_response.channel
-				$sessions[session_id][chatbot_response.channel][chatbot_response.ts] = {} if !$sessions[session_id][chatbot_response.channel].key? chatbot_response.ts
-				# puts "SESSION PRINT: #{$sessions.inspect}" if DEBUG_MODE
-				$sessions[session_id][chatbot_response.channel][chatbot_response.ts][emojis[i]] = reply_text
+		# if response.key?("quickreplies") 
+		# 	# puts response["quickreplies"]
+		# 	index = 1
+		# 	emojis = []
+		# 	message = "Please select one of the following options for further inquiries: \n"
+		# 	for reply in response["quickreplies"] do
+		# 		num_as_emoji = ":#{nums[index]}:"
+		# 		option_str = "#{num_as_emoji} #{reply} \n"
+		# 		message += option_str
+		# 		emojis.push("#{nums[index]}")
+		# 		index += 1
+		# 	end
+		# 	chatbot_response = web_client.chat_postMessage channel: data['channel'], text: "#{message}", as_user: true
+		# 	puts "Chatbot response: #{chatbot_response.ts}"
+		# 	for i in 0...response["quickreplies"].size
+		# 		session_id = data.user
+		# 		reply_text = response["quickreplies"][i] 
+		# 		puts "Adding emoji: #{emojis[i]} on #{chatbot_response.channel} at #{chatbot_response.ts}" if DEBUG_MODE
+		# 		web_client.reactions_add(name: emojis[i], channel: chatbot_response.channel, timestamp: chatbot_response.ts)
+		# 		$sessions[session_id] = {} if !$sessions.key? session_id
+		# 		$sessions[session_id][chatbot_response.channel] = {} if !$sessions[session_id].key? chatbot_response.channel
+		# 		$sessions[session_id][chatbot_response.channel][chatbot_response.ts] = {} if !$sessions[session_id][chatbot_response.channel].key? chatbot_response.ts
+		# 		# puts "SESSION PRINT: #{$sessions.inspect}" if DEBUG_MODE
+		# 		$sessions[session_id][chatbot_response.channel][chatbot_response.ts][emojis[i]] = reply_text
 				
-			end
+		# 	end
 			
-		end
+		# end
 		
 	end
 end
